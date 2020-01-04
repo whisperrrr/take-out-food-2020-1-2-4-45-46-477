5924129bb614a1bb4b17ff3d43048ce2707275a4
JinHui <jh731448520@163.com>
2020-01-04 16:57:54 +0800

    [JinHui]{feat}-{#5.1}:show item list

--- public/index.js
+++ public/index.js
@@ -1,5 +1,27 @@
 // 请把与index.html页面相关的javascript代码写在这里
 // 同时删除该注释
+let itemsListCard = document.getElementById("items");
+let itemsLists = document.createElement("ul");
+let itemsInfo = loadAllItems();
+itemsListCard.appendChild(itemsLists);
+showAllItems(itemsInfo);
+
+//展示所有菜品信息
+function showAllItems(itemsInfo) {
+  let itemsLength = itemsInfo.length;
+  for (let i = 0; i < itemsLength; i++) {
+    let newItemsList = document.createElement("li");
+    let itemsCount = 0;
+    newItemsList.innerHTML = `
+      名称：${itemsInfo[i].name} 
+      单价：${itemsInfo[i].price}
+      <input type="button" value="+" />
+      ${itemsCount}
+      <input type="button" value="-" />
+    `
+    itemsLists.appendChild(newItemsList);  
+  }
+}
 
 function calculatePrice() {
   // 想办法调用`bestCharge`并且把返回的字符串
